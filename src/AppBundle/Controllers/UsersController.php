135afa98688aaaaef976728b8d7785315b0ab5f4
Alexandre HOAREAU <ahoareau@student.42.fr>
2017-07-05 01:53:27 +0200

    add fa + edit profil photo page -- wip

--- src/AppBundle/Controllers/UsersController.php
+++ src/AppBundle/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
     {
         if ($this->isLogged())
         {
-            return $this->app->view->render($response, 'views/users/home.html.twig'), [];
+            return $this->app->view->render($response, 'views/users/'.$args['profil'].'.html.twig', ['app' => new Controller($this->app)]);
         }
         return $this->app->view->render($response, 'views/pages/homepage.html.twig', ['app' => new Controller($this->app)]);
     }
--- src/routes.php
+++ src/routes.php
@@ -2,9 +2,10 @@
 // Routes
 
 $app->get('/', 'PagesController:indexAction')->setName('homepage');
+$app->post('/', 'SecurityController:signInForm')->setName('signInForm');
 $app->get('/signUp', 'SecurityController:signUpAction')->setName('signUp');
-$app->post('/signUp-action', 'SecurityController:signUpForm')->setName('signUpForm');
+$app->post('/signUp', 'SecurityController:signUpForm')->setName('signUpForm');
 $app->get('/logout', 'SecurityController:logout')->setName('logout');
 $app->get('/home', 'UsersController:indexAction')->setName('home');
-$app->post('/', 'SecurityController:signInForm')->setName('signInForm');
+$app->get('/home/{profil}', 'UsersController:indexAction')->setName('edit');
 
